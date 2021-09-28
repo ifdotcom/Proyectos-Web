67a09b5b3cc5bcd2c235ac040e65595f44926276
Fernanda Vidal <ifervb@gmail.com>
2021-09-27 21:20:59 -0500

    Agregando estrucutura de portafolio y clip-path.

---
+++ Navegación/js/script.js
@@ -0,0 +1,15 @@
+html {
+  box-sizing: border-box;
+  font-size: 62.5%; /* Reset para REMS - 62.5% = 10px de 16px */
+}
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: "Lato", sans-serif;
+  font-size: 1.6rem;
+  line-height: 2;
+}
